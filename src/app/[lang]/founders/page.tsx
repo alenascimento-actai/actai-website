31e0ab8782220d5e1fac92cd04a02a4bb14dd86a
Alexandre Nascimento <alexandre.sfi@hotmail.com>
2025-06-17 23:00:24 -0300

    fix: translation in page founders

--- src/app/[lang]/founders/page.tsx
+++ src/app/[lang]/founders/page.tsx
@@ -3,14 +3,13 @@
 import FoundersSection from "@/components/sections/FoundersSection";
 import { getDictionary } from "../dictionaries";
 
-interface PageProps {
-  params: {
-    lang: "en" | "pt-br";
-  };
-}
-
-export default async function FoundersPage({ params }: PageProps) {
-  const dict = await getDictionary(params.lang);
+export default async function FoundersPage({
+  params,
+}: {
+  params: Promise<{ lang: "pt-br" | "en" }>;
+}) {
+  const { lang } = await params;
+  const dict = await getDictionary(lang);
 
   return <FoundersSection dict={dict.founders} />;
 }
